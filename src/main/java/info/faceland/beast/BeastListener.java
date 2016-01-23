cfc46749b02467b25a2c4e486897621f23fc6525
Kyle Wolfgang Kattrein <kattreinkyle@gmail.com>
2016-01-23 05:46:19 -0500

    Better MythicMobs Integration

--- src/main/java/info/faceland/beast/BeastListener.java
+++ src/main/java/info/faceland/beast/BeastListener.java
@@ -26,9 +26,8 @@ import com.tealcube.minecraft.bukkit.TextUtils;
 import com.tealcube.minecraft.bukkit.shade.apache.commons.lang3.math.NumberUtils;
 import com.tealcube.minecraft.bukkit.shade.google.common.base.CharMatcher;
 
-import net.elseland.xikage.MythicMobs.Mobs.ActiveMobHandler;
+import net.elseland.xikage.MythicMobs.Mobs.MythicMob;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -37,8 +36,6 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.entity.PigZombie;
 import org.bukkit.entity.Skeleton;
-import org.bukkit.entity.Slime;
-import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -62,9 +59,9 @@ public final class BeastListener implements Listener {
         this.random = new Random(System.currentTimeMillis());
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onCreatureSpawnHighest(CreatureSpawnEvent event) {
-        if (!ActiveMobHandler.isRegisteredMob(event.getEntity().getUniqueId())) {
+        if (event.getEntity() instanceof MythicMob){
             return;
         }
         BeastData data = plugin.getData(event.getEntity().getType());
@@ -78,96 +75,75 @@ public final class BeastListener implements Listener {
         if (startingLevel < 0) {
             return;
         }
-        double healthMult = 1.0;
         int rank = 0;
-        int level = 0;
-        if (event.getEntity().getCustomName() == null) {
-            Vec2 pos = new Vec2(event.getLocation().getX(), event.getLocation().getZ());
-            Vec2 worldPos = new Vec2(event.getLocation().getWorld().getSpawnLocation().getX(),
-                    event.getLocation().getWorld().getSpawnLocation().getZ());
-            double distanceFromSpawn = pos.distance(worldPos);
-            double pow = plugin.getSettings().getInt("config.enabled-worlds." + event.getLocation().getWorld().getName() +
-                    ".distance-per-level", 150);
-            level = (int) (startingLevel + distanceFromSpawn / pow);
-            level += -2 + random.nextInt(5);
-            level = Math.max(level, 1);
-
-            event.getEntity().getEquipment().clear();
-            if (event.getEntity() instanceof PigZombie) {
-                event.getEntity().getEquipment().setHelmet(new ItemStack(Material.GOLD_HELMET));
-                if (random.nextDouble() < 0.5) {
-                    event.getEntity().getEquipment().setItemInHand(new ItemStack(Material.GOLD_AXE));
-                } else {
-                    event.getEntity().getEquipment().setItemInHand(new ItemStack(Material.GOLD_SWORD));
-                }
-                event.getEntity().getEquipment().setItemInHandDropChance(0f);
-                event.getEntity().getEquipment().setHelmetDropChance(0f);
-            } else if (event.getEntity() instanceof Skeleton) {
-                if (random.nextDouble() < plugin.getSettings().getDouble("config.give-skeletons-sword-chance", 0.1)) {
-                    event.getEntity().getEquipment().setItemInHand(new ItemStack(Material.STONE_SWORD));
-                } else {
-                    event.getEntity().getEquipment().setItemInHand(new ItemStack(Material.BOW));
-                }
-                event.getEntity().getEquipment().setItemInHandDropChance(0f);
-            } else if (event.getEntity() instanceof Slime) {
-                Slime slime = (Slime) event.getEntity();
-                healthMult = slime.getSize() / 4;
-            } else if (event.getEntity() instanceof Wolf) {
-                Wolf wolf = (Wolf) event.getEntity();
-                wolf.setAngry(true);
-            }
-
-            double rankUp = plugin.getSettings().getDouble("config.mob-rankup-chance", 0.1);
-            String rankName = "";
-
-            if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.SPAWNER) {
-                rankName = ChatColor.WHITE + "Spawned ";
-                level += 7;
+        int level = 1;
+        Vec2 pos = new Vec2(event.getLocation().getX(), event.getLocation().getZ());
+        Vec2 worldPos = new Vec2(event.getLocation().getWorld().getSpawnLocation().getX(),
+                event.getLocation().getWorld().getSpawnLocation().getZ());
+        double distanceFromSpawn = pos.distance(worldPos);
+        double pow = plugin.getSettings().getInt("config.enabled-worlds." + event.getLocation().getWorld().getName() +
+                ".distance-per-level", 150);
+        level = (int) (startingLevel + distanceFromSpawn / pow);
+        level += -2 + random.nextInt(5);
+        level = Math.max(level, 1);
+        event.getEntity().getEquipment().clear();
+        if (event.getEntity() instanceof PigZombie) {
+            event.getEntity().getEquipment().setHelmet(new ItemStack(Material.GOLD_HELMET));
+            if (random.nextDouble() < 0.5) {
+                event.getEntity().getEquipment().setItemInHand(new ItemStack(Material.GOLD_AXE));
             } else {
-                while (random.nextDouble() < rankUp && rank < 4) {
-                    rank++;
-                }
-                switch (rank) {
-                    case 0:
-                        rankName = "";
-                        break;
-                    case 1:
-                        rankName = ChatColor.BLUE + "Magic ";
-                        event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 9999, 1, false, false));
-                        break;
-                    case 2:
-                        rankName = ChatColor.DARK_PURPLE + "Rare ";
-                        event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 9999, 1, false, false));
-                        break;
-                    case 3:
-                        rankName = ChatColor.RED + "Epic ";
-                        event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 9999, 1, false, false));
-                        break;
-                    case 4:
-                        rankName = ChatColor.GOLD + "Legendary ";
-                        event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 9999, 1, false, false));
-                        break;
-                }
+                event.getEntity().getEquipment().setItemInHand(new ItemStack(Material.GOLD_SWORD));
             }
-
-            String name = TextUtils.color(TextUtils.args(data.getNameFormat(), new String[][]{{"%level%", String.valueOf(level)}}));
-            name = rankName + name;
-
-            event.getEntity().setCustomName(name);
+            event.getEntity().getEquipment().setItemInHandDropChance(0f);
+            event.getEntity().getEquipment().setHelmetDropChance(0f);
+        } else if (event.getEntity() instanceof Skeleton) {
+            if (random.nextDouble() < plugin.getSettings().getDouble("config.give-skeletons-sword-chance", 0.1)) {
+                event.getEntity().getEquipment().setItemInHand(new ItemStack(Material.STONE_SWORD));
+            } else {
+                event.getEntity().getEquipment().setItemInHand(new ItemStack(Material.BOW));
+            }
+            event.getEntity().getEquipment().setItemInHandDropChance(0f);
+        }
+        double rankUp = plugin.getSettings().getDouble("config.mob-rankup-chance", 0.1);
+        String rankName = "";
+        if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.SPAWNER) {
+            rankName = ChatColor.WHITE + "Spawned ";
+            level += 7;
         } else {
-            level = NumberUtils.toInt(CharMatcher.DIGIT.retainFrom(ChatColor.stripColor(event.getEntity()
-                    .getCustomName())));
-            if (event.getEntity().getCustomName().startsWith(ChatColor.DARK_RED + "Boss")) {
-                rank = 8;
-                event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 9999, 1, false, false));
+            while (random.nextDouble() < rankUp && rank < 4) {
+                rank++;
+            }
+            switch (rank) {
+                case 0:
+                    rankName = "";
+                    break;
+                case 1:
+                    rankName = ChatColor.BLUE + "Magic ";
+                    event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 9999, 1, false, false));
+                    break;
+                case 2:
+                    rankName = ChatColor.DARK_PURPLE + "Rare ";
+                    event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 9999, 1, false, false));
+                    break;
+                case 3:
+                    rankName = ChatColor.RED + "Epic ";
+                    event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 9999, 1, false, false));
+                    break;
+                case 4:
+                    rankName = ChatColor.GOLD + "Legendary ";
+                    event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 9999, 1, false, false));
+                    break;
             }
         }
 
-        double newMaxHealth = healthMult * (1 + (rank * 0.75)) * data.getHealthExpression().setVariable("LEVEL", level)
-                .evaluate();
+        String name = TextUtils.color(TextUtils.args(data.getNameFormat(), new String[][]{{"%level%", String.valueOf(level)}}));
+        name = rankName + name;
+
+        event.getEntity().setCustomName(name);
+
+        double newMaxHealth = (1 + (rank * 0.75)) * data.getHealthExpression().setVariable("LEVEL", level).evaluate();
         double speed = data.getSpeedExpression().setVariable("LEVEL", level).evaluate();
-        event.getEntity().setHealth(Math.min(2, newMaxHealth) / 2);
-        event.getEntity().setMaxHealth(newMaxHealth);
+        event.getEntity().setMaxHealth(Math.max(2, newMaxHealth));
         event.getEntity().setHealth(event.getEntity().getMaxHealth());
         event.getEntity().setCanPickupItems(false);
         event.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 20 * 60 * 10, (int) speed,
